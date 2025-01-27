bf6b5f8e041e253c9fb80ab1fd4f62dc01c31171
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-01-26 22:07:26 -0300

    first commit

---
+++ produtosapi/src/test/java/io/github/eduardoconceicao90/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eduardoconceicao90.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
