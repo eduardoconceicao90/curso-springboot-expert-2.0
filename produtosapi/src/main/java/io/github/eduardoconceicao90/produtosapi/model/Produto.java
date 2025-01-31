e237bd7724739e622894400ef9f04641d1c56535
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-01-30 21:24:39 -0300

    Criando o mapeamento JPA para entidade Produto

--- produtosapi/src/main/java/io/github/eduardoconceicao90/produtosapi/model/Produto.java
+++ produtosapi/src/main/java/io/github/eduardoconceicao90/produtosapi/model/Produto.java
@@ -1,10 +1,28 @@
 package io.github.eduardoconceicao90.produtosapi.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "produto")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
     private String id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco")
     private Double preco;
 
 }
