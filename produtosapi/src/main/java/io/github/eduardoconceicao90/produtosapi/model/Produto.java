860fb30841375c2a73ff5970e0913142d0319bb4
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-01-30 21:09:34 -0300

    Efetuando conexão para o banco H2 com extensão .yml

--- produtosapi/src/main/java/io/github/eduardoconceicao90/produtosapi/model/Produto.java
+++ produtosapi/src/main/java/io/github/eduardoconceicao90/produtosapi/model/Produto.java
@@ -1,14 +1,5 @@
 package io.github.eduardoconceicao90.produtosapi.model;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@Entity(name = "produto")
 public class Produto {
 
     private String id;
