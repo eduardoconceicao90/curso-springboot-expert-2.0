555df49a78dc0b38dc2ba268dbbddd5b029af1e2
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-02-01 09:54:31 -0300

    Obtendo dados e excluido produtos

--- produtosapi/src/main/java/io/github/eduardoconceicao90/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/eduardoconceicao90/produtosapi/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
@@ -19,6 +20,21 @@ public class ProdutoController {
     @ResponseStatus(HttpStatus.CREATED)
     public Produto save(@RequestBody Produto produto){
         return produtoRepository.save(produto);
+    }
+
+    @GetMapping("/{id}")
+    public Produto getProdutoById(@PathVariable String id){
+        return produtoRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado!"));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String id){
+        produtoRepository.findById(id).map(produto -> {
+                    produtoRepository.delete(produto);
+                    return produto;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado!"));
     }
 
 }
