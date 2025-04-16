deba5b22c42689f8dd6271220842969e816af09e
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-04-16 09:23:59 -0300

    Ajuste no Security Service

--- libraryapi/src/main/java/io/github/eduardoconceicao90/libraryapi/security/SecurityService.java
+++ libraryapi/src/main/java/io/github/eduardoconceicao90/libraryapi/security/SecurityService.java
@@ -5,7 +5,6 @@ import io.github.eduardoconceicao90.libraryapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -16,8 +15,12 @@ public class SecurityService {
 
     public Usuario obterUsuarioLogado() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String login = userDetails.getUsername();
-        return usuarioService.obterPorLogin(login);
+
+        if(authentication instanceof CustomAuthentication customAuthentication) {
+            return customAuthentication.getUsuario();
+        }
+
+        return null;
     }
+
 }
