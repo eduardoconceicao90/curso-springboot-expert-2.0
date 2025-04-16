8c50296398a89c815f758306b210a595c0e5c89e
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-04-16 10:00:08 -0300

    Adicionando campo para salvar email no cadastro do usuário

--- libraryapi/src/main/java/io/github/eduardoconceicao90/libraryapi/controller/UsuarioController.java
+++ libraryapi/src/main/java/io/github/eduardoconceicao90/libraryapi/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package io.github.eduardoconceicao90.libraryapi.controller;
 import io.github.eduardoconceicao90.libraryapi.controller.mapper.UsuarioMapper;
 import io.github.eduardoconceicao90.libraryapi.model.dto.UsuarioDTO;
 import io.github.eduardoconceicao90.libraryapi.service.UsuarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ public class UsuarioController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void salvar(@RequestBody UsuarioDTO dto){
+    public void salvar(@RequestBody @Valid UsuarioDTO dto){
         var usuario = mapper.toEntity(dto);
         service.salvar(usuario);
     }
--- libraryapi/src/main/java/io/github/eduardoconceicao90/libraryapi/model/dto/UsuarioDTO.java
+++ libraryapi/src/main/java/io/github/eduardoconceicao90/libraryapi/model/dto/UsuarioDTO.java
@@ -1,10 +1,20 @@
 package io.github.eduardoconceicao90.libraryapi.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 
 public record UsuarioDTO(
+        @NotBlank(message = "Campo login é obrigatório")
         String login,
+
+        @NotBlank(message = "Campo senha é obrigatório")
         String senha,
+
+        @Email(message = "Email inválido")
+        String email,
+
         List<String> roles
 ) {
 }
