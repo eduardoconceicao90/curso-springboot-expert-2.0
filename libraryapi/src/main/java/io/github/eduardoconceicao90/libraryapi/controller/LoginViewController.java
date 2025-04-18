52ecf618434e263266d1aba71b4becb462aa9d72
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-04-18 19:02:07 -0300

    Autenticando com Authorization Code grant type

--- libraryapi/src/main/java/io/github/eduardoconceicao90/libraryapi/controller/LoginViewController.java
+++ libraryapi/src/main/java/io/github/eduardoconceicao90/libraryapi/controller/LoginViewController.java
@@ -4,6 +4,7 @@ import io.github.eduardoconceicao90.libraryapi.security.CustomAuthentication;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -23,4 +24,11 @@ public class LoginViewController {
 
         return "Olá " + authentication.getName();
     }
+
+    @GetMapping("/authorized")
+    @ResponseBody
+    public String getAuthorizationCode(@RequestParam String code) {
+        return "Seu authorization code é: " + code;
+    }
+
 }
